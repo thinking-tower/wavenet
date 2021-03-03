a7d74d31d69fd5a397f92f6e2a314aaca4bde90b
Darius Tan <dariustan97@gmail.com>
2021-03-03 19:11:32 +1300

    Add resources. Implement causal dilated convolution.

--- src/layers/dilated_convolution.py
+++ src/layers/dilated_convolution.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 
-class CausalConvolution1d(nn.Module):
+class CausalDilatedConvolution1d(nn.Module):
     def __init__(
         self,
         in_channels: int,
@@ -26,7 +26,36 @@ class CausalConvolution1d(nn.Module):
             bias=bias,
             padding_mode=padding_mode
         )
-    def forward(self, x):
-        out = self.dilated_convolution(x)
-        # TODO:
-        # 1. Shift output forward by one so that prediction for timestep t uses only up to the t-1 sample
+        self.kernel_size = self.dilated_convolution.weight.shape[-1]
+
+    def forward(self, input_):
+        """
+        Consider the following output for a non-causal convolution:
+
+        output:             1 2
+                           /|/|
+        hidden_layer:     1 2 3  
+                         /|/|/|
+        input_:         1 2 3 4
+
+        For output_2 to not see input_4 (the current timestep we're trying to predict), 
+        we should shift the input to hidden layer forward by 1.  
+
+        output:             1 2
+                           /|/|
+        hidden_layer:       1 2 3 (3 is discarded)
+                           / / /
+                          | | |
+                         /|/|/|
+        input_:         1 2 3 4
+        Now both output_2 and output_1 can't see the input at the current timestep but can see all before it.
+        See resources/WO2018048934A1.pdf, resources/iclr2017.pdf, resources/iclr2017.pdf.
+
+        Args:
+            input_ (torch.Tensor): (Batches, Channels, Width)
+
+        Returns:
+            torch.Tensor: (Batches, Channels, Width)
+        """
+        output = self.dilated_convolution(input_)
+        return output[:, :, -(self.kernel_size-1)]
